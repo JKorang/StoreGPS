9819a737ae18312bc0915b9eaa24a9fd4401838c
Brian <brosenblum608@gmail.com>
2015-04-23 17:47:38 -0400

    Changed a couple lines of code for easier implementation of observer pattern

--- app/src/main/java/com/rowansenior/storegps/ListAdapter.java
+++ app/src/main/java/com/rowansenior/storegps/ListAdapter.java
@@ -53,7 +53,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
         ShoppingListItem item = items.get(i);
         viewHolder.vTitleText.setText(item.getName());
         viewHolder.vQuantityInt = item.getQuantity();
-        viewHolder.vQuantity.setText("Quantity: " + String.valueOf(item.getQuantity()));
+        viewHolder.vQuantity.setText("Quantity: " + String.valueOf(viewHolder.vQuantityInt));
         System.out.println("IS NAV??? " + viewHolder.vIsNavigated);
         if(viewHolder.vIsNavigated == true) {
             viewHolder.vItemLocation.setText("Yeah this works?");
@@ -115,6 +115,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
             switch (v.getId()) {
                 case R.id.incQuantity:
                     db.increaseQuantity(parentList, vTitleText.getText().toString());
+                    vQuantityInt++;
                     break;
                 case R.id.decQuantity:
                     if(vQuantityInt < 2)
